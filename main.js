7f61664b5ebb48e2ecc5a9aba8296e71b574f50b
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-03-17 21:35:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,8 +21,33 @@ function model_loaded(){
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is" + prediction_1;
-    speak_data_2 = "And the second prediction is" + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    speak_data = "The first prediction is" + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
 }
+function predict(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("gesture").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+      if(results[0].label == "Amazing"){
+           document.getElementById("gesture_image").innerHTML = "&#128076";
+           console.log("Amazing");
+     }
+     if(results[0].label == "Best"){
+        document.getElementById("gesture_image").innerHTML = "&#128077";
+        console.log("Best");
+     }
+     if(results[0].label == "Victory"){
+        document.getElementById("gesture_image").innerHTML = "&#9996";
+        console.log("Victory");
+     }
+   }
+ }
